f68934be13359e1e4b78765e4b857a315df8ebc8
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-10 14:17:25 +0330

    router added for users sign in

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,10 +2,10 @@ const express = require('express');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const router = express.Router();
-const {User, userValidate} = require("../models/user");
+const {User, userSignUpValidate, userSignInValidate } = require("../models/user");
 
 router.post('/sign-up', async (req, res) => {
-    const {error} = userValidate(req.body);
+    const {error} = userSignUpValidate(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
     let user = await User.findOne({
@@ -26,5 +26,24 @@ router.post('/sign-up', async (req, res) => {
     res.cookie('x-auth-token', token, { httpOnly: true });
     res.send(_.pick(user, ['id', 'name', 'lastName', 'email', 'phoneNumber']));
 });
+
+router.post('/sign-in', async (req, res) => {
+    const {error} = userSignInValidate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findOne({
+        where: {
+            email: req.body.email
+        }
+    });
+    if (!user) return res.status(400).send('Invalid email or password.');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(400).send('Invalid email or password.');
+
+    const token = user.generateAuthToken();
+    res.cookie('x-auth-token', token, { httpOnly: true });
+    res.send(_.pick(user, ['id', 'name', 'lastName', 'email', 'phoneNumber']));
+})
 
 module.exports = router;
