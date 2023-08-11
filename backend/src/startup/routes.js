4e1538a3a401561ebc5dccd6a6ab7991a83909b2
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-11 16:48:58 +0330

    helmet and cors middleware added to here.

--- backend/src/startup/routes.js
+++ backend/src/startup/routes.js
@@ -3,8 +3,11 @@ const cookieParser = require('cookie-parser');
 const test = require('../routes/test');
 const users = require('../routes/users');
 const error = require('../middlewares/error');
-
+const helmet = require('helmet');
+const cors = require('cors');
 module.exports = function (app){
+    app.use(helmet());
+    app.use(cors())
     app.use(express.json());
     app.use(cookieParser());
     app.use('/api/test', test);
