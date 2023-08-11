6b4f30c8fb694b8b3649ac736a3891e8bb093e67
NYXTheShadow <alielahirad8087@gmail.com>
2023-08-11 09:49:27 +0330

    Made form container responsive.

--- frontend/src/components/FormContainer.tsx
+++ frontend/src/components/FormContainer.tsx
@@ -1,37 +1,41 @@
-import { Center, VStack, useColorModeValue } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import {Center, VStack, useColorModeValue} from "@chakra-ui/react";
+import {ReactNode} from "react";
 import Blur from "./Blur";
 
 interface Props {
-  children: ReactNode | ReactNode[];
+    children: ReactNode | ReactNode[];
 }
 
-const FormContainer = ({ children }: Props) => {
-  return (
-    <>
-      <Center>
-        <VStack
-          padding={10}
-          marginTop={10}
-          borderRadius={15}
-          borderWidth="1px"
-          borderStyle="solid"
-          borderColor={useColorModeValue("gray.200", "gray.700")}
-          boxShadow="lg"
-          bgColor={useColorModeValue("white", "gray.800")}
-          width="30vw"
-        >
-          {children}
-        </VStack>
-      </Center>
-      <Blur
-        position="absolute"
-        top="120px"
-        right={-10}
-        style={{ filter: "blur(70px)" }}
-      />
-    </>
-  );
+const FormContainer = ({children}: Props) => {
+    return (
+        <>
+            <Center>
+                <VStack
+                    padding={10}
+                    marginTop={10}
+                    borderRadius={15}
+                    borderWidth="1px"
+                    borderStyle="solid"
+                    borderColor={useColorModeValue("gray.200", "gray.700")}
+                    boxShadow="lg"
+                    bgColor={useColorModeValue("white", "gray.800")}
+                    width={{
+                        base: '80vw',
+                        md: '50vw',
+                        lg: '35vw'
+                    }}
+                >
+                    {children}
+                </VStack>
+            </Center>
+            <Blur
+                position="absolute"
+                top="120px"
+                right={-10}
+                style={{filter: "blur(70px)"}}
+            />
+        </>
+    );
 };
 
 export default FormContainer;
