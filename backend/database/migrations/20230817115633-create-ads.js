160c16e0989c1c4ff12b28ba877c21006acc8de2
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-17 16:15:42 +0330

    migration added for creating ads table.

---
+++ backend/database/migrations/20230817115633-create-ads.js
@@ -0,0 +1,48 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('ads', {
+            id: {
+                type: Sequelize.DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            title: {
+                type: Sequelize.DataTypes.STRING
+            },
+            description: {
+                type: Sequelize.DataTypes.STRING
+            },
+            location : {
+                type: Sequelize.DataTypes.STRING
+            },
+            SuggestedPrice: {
+                type: Sequelize.DataTypes.STRING
+            },
+            isClosed: {
+                type: Sequelize.DataTypes.BOOLEAN
+            },
+            categoryId: {
+                type: Sequelize.DataTypes.INTEGER,
+                references: {
+                    model: 'categories',
+                    key: 'id',
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            },
+            createdAt: {
+                type: Sequelize.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.DataTypes.NOW,
+            },
+            updatedAt: {
+                type: Sequelize.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.DataTypes.NOW,
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('ads');
+    }
+}
