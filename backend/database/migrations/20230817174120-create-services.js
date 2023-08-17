d858338a4e665cc95e5246ca1baa123c9dce9947
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-17 21:14:47 +0330

    migration created for creating services table.

---
+++ backend/database/migrations/20230817174120-create-services.js
@@ -0,0 +1,36 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('services', {
+            id: {
+                type: Sequelize.DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            name: {
+                type: Sequelize.DataTypes.STRING
+            },
+            categoryId: {
+                type: Sequelize.DataTypes.INTEGER,
+                references: {
+                    model: 'categories',
+                    key: 'id',
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            },
+            createdAt: {
+                type: Sequelize.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.DataTypes.NOW,
+            },
+            updatedAt: {
+                type: Sequelize.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.DataTypes.NOW,
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('services');
+    }
+}
