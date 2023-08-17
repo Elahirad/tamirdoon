c878aec41a2cc623f33bae5e1cc9cc15894ce7d2
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-17 16:13:34 +0330

    association with Image added and a validation added for location.

--- backend/src/models/ad.js
+++ backend/src/models/ad.js
@@ -1,6 +1,8 @@
 const {DataTypes} = require("sequelize");
 const {sequelize} = require("../../config/db.js");
 const {User} = require('./user');
+const {Image} = require('./image');
+
 
 const Ad = sequelize.define('Ad', {
     title: {
@@ -10,7 +12,17 @@ const Ad = sequelize.define('Ad', {
         type: DataTypes.STRING
     },
     location : {
-        type: DataTypes.STRING
+        type: DataTypes.JSON,
+        validate: {
+            isValidObject(value) {
+                if (!value || typeof value !== 'object') {
+                    throw new Error('Invalid location object');
+                }
+                if (typeof value.latitude !== 'number' || typeof value.longitude !== 'number') {
+                    throw new Error('Missing required keys in location object');
+                }
+            },
+        }
     },
     SuggestedPrice: {
         type: DataTypes.STRING
@@ -22,5 +34,8 @@ const Ad = sequelize.define('Ad', {
 
 User.hasMany(Ad);
 Ad.belongsTo(User);
+
+Ad.belongsToMany(Image, { through: 'adImages' });
+Image.belongsToMany(Ad, { through: 'adImages' });
 
 module.exports = Ad;
