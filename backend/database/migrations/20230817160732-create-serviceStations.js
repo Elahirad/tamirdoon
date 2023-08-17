77d3685a25a88cadfdfe0ab9004dc278f352ea55
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-17 20:16:31 +0330

    migration added for creating service station table.

---
+++ backend/database/migrations/20230817160732-create-serviceStations.js
@@ -0,0 +1,38 @@
+module.exports = {
+    async up (queryInterface, Sequelize) {
+        await queryInterface.createTable('serviceStations', {
+            id: {
+                type: Sequelize.DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            name: {
+                type: Sequelize.DataTypes.STRING,
+                allowNull: false,
+            },
+            location : {
+                type: Sequelize.DataTypes.JSON
+            },
+            phoneNumber: {
+                type: Sequelize.DataTypes.STRING
+            },
+            workingHoursDescription: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            createdAt: {
+                type: Sequelize.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.DataTypes.NOW,
+            },
+            updatedAt: {
+                type: Sequelize.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.DataTypes.NOW,
+            }
+        });
+    },
+    async down (queryInterface, Sequelize) {
+        await queryInterface.dropTable('serviceStations');
+  }
+};
