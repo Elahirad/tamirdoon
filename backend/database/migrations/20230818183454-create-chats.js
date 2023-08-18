02cee332cc92a7d38132d23550fbfd4076b8d8a0
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-18 22:12:05 +0330

    migration added for create chats table.

---
+++ backend/database/migrations/20230818183454-create-chats.js
@@ -0,0 +1,59 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('chats', {
+            id: {
+                type: Sequelize.DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            message:{
+                type: Sequelize.DataTypes.STRING,
+                allowNull: false
+            },
+            isRead:{
+                type: Sequelize.DataTypes.BOOLEAN,
+                default: false,
+            },
+            to: {
+                type: Sequelize.DataTypes.INTEGER,
+                references: {
+                    model: 'clients',
+                    key: 'id',
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            },
+            from: {
+                type: Sequelize.DataTypes.INTEGER,
+                references: {
+                    model: 'clients',
+                    key: 'id',
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            },
+            replyTo: {
+                type: Sequelize.DataTypes.INTEGER,
+                references: {
+                    model: 'chats',
+                    key: 'id',
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            },
+            createdAt: {
+                type: Sequelize.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.DataTypes.NOW,
+            },
+            updatedAt: {
+                type: Sequelize.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.DataTypes.NOW,
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('chats');
+    }
+}
