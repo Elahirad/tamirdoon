db00fd96ec17163ba81cba1bbd4084f63d2ff08c
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-18 19:53:46 +0330

    migration created for creating admins table.

---
+++ backend/database/migrations/20230818161946-create-admins.js
@@ -0,0 +1,72 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('admins', {
+            id: {
+                type: Sequelize.DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            firstName: {
+                type: Sequelize.DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    min: 2,
+                    max: 50
+                }
+            },
+            lastName:{
+                type: Sequelize.DataTypes.STRING,
+                validate: {
+                    min: 2,
+                    max: 50
+                }
+            },
+            phoneNumber: {
+                type: Sequelize.DataTypes.STRING
+            },
+            email: {
+                type: Sequelize.DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    isEmail: true
+                }
+            },
+            password: {
+                type: Sequelize.DataTypes.STRING,
+                allowNull: false
+            },
+            createdAt: {
+                type: Sequelize.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.DataTypes.NOW,
+            },
+            updatedAt: {
+                type: Sequelize.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.DataTypes.NOW,
+            },
+            ImageId: {
+                type: Sequelize.DataTypes.INTEGER,
+                references: {
+                    model: 'images',
+                    key: 'id',
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            },
+            ClientId: {
+                type: Sequelize.DataTypes.INTEGER,
+                references: {
+                    model: 'clients',
+                    key: 'id',
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('admins');
+    }
+}
