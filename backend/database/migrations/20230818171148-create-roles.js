814c5fd8fd14c03c43fe6649d334df669a93b76e
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-18 20:48:06 +0330

    migration created for creating roles table

---
+++ backend/database/migrations/20230818171148-create-roles.js
@@ -0,0 +1,28 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('roles', {
+            id: {
+                type: Sequelize.DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            name: {
+                type: Sequelize.DataTypes.STRING,
+                allowNull: false,
+            },
+            createdAt: {
+                type: Sequelize.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.DataTypes.NOW,
+            },
+            updatedAt: {
+                type: Sequelize.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.DataTypes.NOW,
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('roles');
+    }
+}
