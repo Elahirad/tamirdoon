c9b45338d55329b02d154688e43834f8e9c91308
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-21 19:41:42 +0330

    comment model added.

---
+++ backend/src/models/comment.js
@@ -0,0 +1,38 @@
+const {DataTypes} = require("sequelize");
+const {sequelize} = require("../../config/db.js");
+const { Customer } = require('../models/customer');
+const {Serviceman} = require("./serviceman");
+
+const Comment = sequelize.define('Comment', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    positivePoints: {
+        type: DataTypes.ARRAY(DataTypes.STRING)
+    },
+    negativePoints: {
+        type: DataTypes.ARRAY(DataTypes.STRING)
+    },
+    isAccepted: {
+        type: DataTypes.BOOLEAN
+    },
+    isRecommended: {
+        type: DataTypes.BOOLEAN
+    }
+});
+
+Customer.hasMany(Comment, { foreignKey: 'customerId' });
+Comment.belongsTo(Customer, { foreignKey: 'customerId' });
+
+Serviceman.hasMany(Comment, { foreignKey: 'servicemanId' });
+Comment.belongsTo(Serviceman, { foreignKey: 'servicemanId' });
+
+module.exports = Comment;
+
+
+
