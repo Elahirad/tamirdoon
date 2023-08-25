8271afb18f46a5d9e6656a86d68d02abb258b2f8
NYXTheShadow <alielahirad8087@gmail.com>
2023-08-25 13:51:35 +0330

    Added users table.

--- frontend/src/pages/Admin/Panel/AdminPanelUsersPage.tsx
+++ frontend/src/pages/Admin/Panel/AdminPanelUsersPage.tsx
@@ -1,7 +1,96 @@
-import { Text } from "@chakra-ui/react";
+import {
+	Heading,
+	Icon,
+	IconButton,
+	Table,
+	TableContainer,
+	Tbody,
+	Td,
+	Th,
+	Thead,
+	Tr,
+	VStack,
+} from '@chakra-ui/react';
+import {MdOutlineVerified} from 'react-icons/md';
+import {BiEditAlt} from 'react-icons/bi';
+import {MdDelete} from 'react-icons/md';
 
 const AdminPanelUsersPage = () => {
-  return <Text>صفحه کاربران</Text>;
+	return (
+		<VStack width="100%" align="flex-start">
+			<Heading as="h2" mt={5} mr={3}>
+				مدیریت کاربران
+			</Heading>
+			<TableContainer width="100%" p={10}>
+				<Table variant="simple">
+					<Thead>
+						<Tr>
+							<Th>#</Th>
+							<Th>نام</Th>
+							<Th>نام خانوادگی</Th>
+							<Th>ایمیل</Th>
+							<Th>شماره تلفن</Th>
+							<Th>
+								<Icon as={BiEditAlt} />
+							</Th>
+							<Th>
+								<Icon as={MdDelete} />
+							</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						<Tr>
+							<Td>1</Td>
+							<Td>علی</Td>
+							<Td>الهی راد</Td>
+							<Td>
+								alielahirad8087@gmail.com{' '}
+								<Icon as={MdOutlineVerified} color="green.400" />
+							</Td>
+							<Td>
+								09152620887 <Icon as={MdOutlineVerified} color="green.400" />
+							</Td>
+							<Td>
+								<IconButton
+									aria-label="edit-user"
+									icon={<BiEditAlt />}
+									colorScheme="blue"
+								/>
+							</Td>
+							<Td>
+								<IconButton
+									aria-label="delete-user"
+									icon={<MdDelete />}
+									colorScheme="red"
+								/>
+							</Td>
+						</Tr>
+						<Tr>
+							<Td>2</Td>
+							<Td>امیرحسین</Td>
+							<Td>فیروزی</Td>
+							<Td>amirfrz@gmail.com</Td>
+							<Td></Td>
+							<Td>
+								<IconButton
+									aria-label="edit-user"
+									icon={<BiEditAlt />}
+									colorScheme="blue"
+								/>
+							</Td>
+							<Td>
+								<IconButton
+									aria-label="delete-user"
+									icon={<MdDelete />}
+									colorScheme="red"
+								/>
+							</Td>
+						</Tr>
+					</Tbody>
+				</Table>
+			</TableContainer>
+		</VStack>
+	);
 };
 
 export default AdminPanelUsersPage;
