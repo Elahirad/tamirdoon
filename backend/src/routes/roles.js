28059baad3d521d342a3bbed6c8988d239e28a8e
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-26 14:34:26 +0330

    put and delete methods added.

--- backend/src/routes/roles.js
+++ backend/src/routes/roles.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { Role, roleCreateValidate } = require('../models/role');
+const { Role, roleCreateValidate, roleUpdateValidate } = require('../models/role');
+const {Permission} = require("../models/permission");
 
 router.get('/', async (req, res) => {
     res.send(await Role.findAll());
@@ -16,7 +17,50 @@ router.post('/create', async (req, res) => { // needs middleware to do this
     if (role) return res.status(400).send("Role already exists.");
 
     role = await Role.create({name: req.body.name});
+
+    for(const id of req.body.addPermissionIds) {
+        const permission = await Permission.findByPk(id);
+        await role.addPermission(permission);
+    }
+
     res.send(role);
 });
+
+router.put('/:id', async (req, res) => {
+    const {error} = roleUpdateValidate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    console.log(req.params.id);
+    let role = await Role.findByPk(req.params.id)
+    if (!role) return res.status(400).send("Role doesn't exist.");
+
+    if(req.body.name) role.name = req.body.name;
+
+    if(req.body.addPermissionIds)
+        for(const id of req.body.addPermissionIds) {
+            const permission = await Permission.findByPk(id);
+            await role.addPermission(permission);
+        }
+
+    if(req.body.removePermissionIds)
+        for(const id of req.body.removePermissionIds) {
+            const permission = await Permission.findByPk(id);
+            await role.removePermission(permission);
+        }
+
+    await role.save();
+    res.send(role);
+});
+
+router.delete('/:id', async (req, res) => {
+    const role = await Role.findOne({
+        where: { id: req.params.id}
+    });
+    if (!role) return res.status(400).send("Role doesn't exist.");
+
+    await role.destroy();
+
+    res.send('deleted successfully.')
+
+})
 
 module.exports = router;
