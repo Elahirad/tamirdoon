2bcde799a774b54ad687355129d174ad98d42306
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-26 14:33:55 +0330

    new joi validation created.

--- backend/src/models/role.js
+++ backend/src/models/role.js
@@ -16,8 +16,18 @@ Admin.belongsToMany(Role, { through: 'adminRoles', foreignKey : 'adminId' });
 function roleCreateValidate(role) {
     const schema = Joi.object({
         name: Joi.string().required(),
+        addPermissionIds: Joi.array().items(Joi.number().integer()).required(),
     });
     return schema.validate(role);
 }
 
-module.exports = { Role, roleCreateValidate};
+function roleUpdateValidate(role) {
+    const schema = Joi.object({
+        name: Joi.string(),
+        addPermissionIds: Joi.array().items(Joi.number().integer()),
+        removePermissionIds: Joi.array().items(Joi.number().integer())
+    });
+    return schema.validate(role);
+}
+
+module.exports = { Role, roleCreateValidate, roleUpdateValidate};
