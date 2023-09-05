0e552e6ddc98df0c76f8b1e944fd0e254ad0cf3f
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-09-05 17:50:22 +0330

    fixed.

--- backend/src/middlewares/adminAuth.js
+++ backend/src/middlewares/adminAuth.js
@@ -19,8 +19,8 @@ module.exports = function (permission){
             if (permission) {
                 const admin = await Admin.findByPk(req.admin.id);
                 const role = await admin.getRoles();
-                let permissions = await role.getPermissions();
-                permissions = permissions.map((p) => p.name);
+                let permissions = await role[0].getPermissions();
+                permissions = permissions.map((p) => p.code);
                 if(permissions.includes(permission) || permissions.includes('ALL_' + permission.split('_')[1])) next();
                 else
                     return res.sendStatus(403); // Forbidden
