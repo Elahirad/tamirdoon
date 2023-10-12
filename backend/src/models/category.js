aca6082011c549786f59d1a89bc789edca132672
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-10-12 15:04:20 +0330

    validation function added to category model.

--- backend/src/models/category.js
+++ backend/src/models/category.js
@@ -1,5 +1,6 @@
 const {DataTypes} = require("sequelize");
 const {sequelize} = require("../../config/db.js");
+const Joi = require("joi");
 
 const Category = sequelize.define('Category', {
     name: {
@@ -8,5 +9,12 @@ const Category = sequelize.define('Category', {
     }
 }, {tableName: 'categories'});
 
+function categoryValidate(category) {
+    const schema = Joi.object({
+        name: Joi.string().required()
+    });
+    return schema.validate(category);
+}
 
-module.exports = Category;
+
+module.exports = {Category, categoryValidate};
